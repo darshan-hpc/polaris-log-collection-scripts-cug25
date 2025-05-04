dd6ee00b32101e9fe16d36f57d6f1a2795f6c53e
Shane Snyder <ssnyder@mcs.anl.gov>
2025-05-04 13:41:09 -0500

    add script for case study 1

---
+++ log-collection-analysis/case-study-1/find-interesting-jobs-mpiio.py
@@ -0,0 +1,19 @@
+import sys
+import pandas as pd
+
+if len(sys.argv) != 2:
+    print(f"Usage: {sys.argv[0]} <job_stats_dir>")
+    sys.exit(1)
+job_stats_dir = sys.argv[1]
+
+# read input MPI-IO data
+mpiio_df = pd.read_csv(f'{job_stats_dir}/mpiio-all.csv')
+ 
+# filter out jobs doing small amounts of I/O (less than 1 GiB)
+mpiio_df = mpiio_df[mpiio_df['total_bytes'] > (1024**3)]
+
+# resort data to be in ascending performance (default for job_stats is ascending total_bytes)
+mpiio_df = mpiio_df.sort_values(by='perf_by_slowest')
+
+# print out 10 lowest performing jobs to inspect manually
+print(mpiio_df.head(10).to_string())
